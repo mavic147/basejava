b541dfae4140013f00c150bb57aff4a8d3c6295f
mavic147 <vamaximenko104@gmail.com>
2020-08-29 15:19:25 +0300

    fixed

--- src/com/basejava/lesson1/ArrayStorage.java
+++ src/com/basejava/lesson1/ArrayStorage.java
@@ -9,32 +9,37 @@ public class ArrayStorage {
     int id;
 
     public void add() {
+        id = 1;
         System.out.print("How many employees would you like to add?: ");
         int employeeAmount = scan.nextInt();
-        for (id = 1; id <= employeeAmount; id++) {
+        for (int i = id - 1; i < employeeAmount; i++) {
             Resume resume = new Resume();
-            resumeArr[id] = resume;
+            resumeArr[i] = resume;
+            id++;
+            System.out.println();
         }
     }
 
     public void get() {
         System.out.print("Print the id-number of the employee, who you would like to read about: ");
         id = scan.nextInt();
-        System.out.println(resumeArr[id].getName());
-        System.out.println(resumeArr[id].getSurname());
-        System.out.println(resumeArr[id].getAge());
-        System.out.println(resumeArr[id].getProfession());
-        System.out.println(resumeArr[id].getExperience());
+        System.out.println(resumeArr[id - 1].getName());
+        System.out.println(resumeArr[id - 1].getSurname());
+        System.out.println(resumeArr[id - 1].getAge());
+        System.out.println(resumeArr[id - 1].getProfession());
+        System.out.println(resumeArr[id - 1].getExperience());
+        System.out.println();
     }
 
     public void getAll() {
         System.out.println(Arrays.toString(resumeArr));
+        System.out.println();
     }
 
     public void delete() {
         System.out.print("Print the number of the employee, whose info you would like to delete: ");
         id = scan.nextInt();
-        resumeArr[id] = null;
+        resumeArr[id - 1] = null;
         for (int curInd = id + 1; curInd < resumeArr.length; curInd++) {
             if (resumeArr[id] == null & resumeArr[curInd] != null) {
                 resumeArr[id] = resumeArr[curInd];
@@ -44,18 +49,21 @@ public class ArrayStorage {
         getAll();
     }
 
-    public int size() {
+    public void size() {
         int count = 0;
-        id = 1;
+        id = 0;
         while (id < resumeArr.length) {
             if (resumeArr[id] != null) {
                 count++;
             }
+            id++;
         }
-        return count;
+        System.out.println("The amount of employees filled " + count);
+        System.out.println();
     }
 
     public void clear() {
         Arrays.fill(resumeArr, null);
+        getAll();
     }
 }
